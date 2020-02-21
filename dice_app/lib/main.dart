47adba1c9a687a448424ab8d578d561d92d22403
Marcus Kalmár <marcus@Designers-MBP.localdomain>
2020-02-21 17:05:46 +0100

    Create stateless widget that randomises the dice

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -17,12 +16,46 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  _DicePageState createState() => _DicePageState();
 }
 
+class _DicePageState extends State<DicePage> {
 
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1; 
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   return Center(child: Row(
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+                changeDiceFace();
+            },
+            child: Image.asset('images/$leftDiceNumber.png'),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              changeDiceFace();
+            },
+            child: Image.asset('images/$rightDiceNumber.png'),
+          ),
+        ),
+        ],
+      ),
+    );
+  }
+}
 
